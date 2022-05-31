015c63918e5fce68c2df159c50bc84e7c778445f
Paulo Coronado <plcpinho@gmail.com>
2022-05-31 15:47:39 -0300

    (feat): update transaction

---
+++ updateTransaction.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', async () => {
+	try {
+		const searchParams = new URLSearchParams(window.location.search);
+
+		if (!searchParams.has('session_id')) throw Error('Unable to update data');
+
+		const data = { checkout_session_id: searchParams.get('session_id') };
+
+		const response = await fetch(
+			'https://landera-network-7ikj4ovbfa-uc.a.run.app/api/v1/transactions',
+			{
+				method: 'patch',
+				body: data,
+			}
+		);
+
+		const responseData = await response.json();
+
+		if (!Object.keys(responseData).length) throw Error('Unable to update data');
+	} catch (error) {
+		window.location.replace('nft/error');
+	}
+});
