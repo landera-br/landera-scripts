ae1e4f29356e6ad3d34cb5c8e4e618ff151b6125
Paulo Coronado <plcpinho@gmail.com>
2022-05-31 10:26:24 -0300

    (test): formdata

--- form.js
+++ form.js
@@ -435,9 +435,33 @@ function getFormData() {
 		},
 	};
 
-	$.each(data, (key, value) => {
-		if (value) formData.append(key, JSON.stringify(value));
-	});
+	formData = objectToFormData(data, formData);
+
+	// $.each(data, (key, value) => {
+	// 	if (value) formData.append(key, JSON.stringify(value));
+	// });
+}
+
+function objectToFormData(obj, form) {
+	var fd = form || new FormData();
+	var formKey;
+
+	for (var property in obj) {
+		if (obj.hasOwnProperty(property)) {
+			formKey = property;
+
+			// if the property is an object, but not a File,
+			// use recursivity.
+			if (typeof obj[property] === 'object' && !(obj[property] instanceof File)) {
+				objectToFormData(obj[property], fd, property);
+			} else {
+				// if it's a string or a File object
+				fd.append(formKey, obj[property]);
+			}
+		}
+	}
+
+	return fd;
 }
 
 function cleanFurniture() {
