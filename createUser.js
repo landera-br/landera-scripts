7b59c85d70d4f56dc9eddee48eba9f9531ee4ce0
Paulo Coronado <plcpinho@gmail.com>
2022-06-06 11:34:33 -0300

    (feat): create user

---
+++ createUser.js
@@ -0,0 +1,40 @@
+// NOTE When form is submitted
+$('#btn-submit').on('click', async (e) => {
+	e.preventDefault();
+
+	// NOTE Get form data
+	const data = {
+		name: $('#field-name').val(),
+		email: $('#field-email').val(),
+		creci: $('#field-creci').val(),
+		phone: $('#field-phone').val(),
+		plan: $('#select-plan').val(),
+	};
+
+	console.log(data);
+
+	try {
+		const response = await fetch('https://landera-network-7ikj4ovbfa-uc.a.run.app/api/v1/users', {
+			method: 'post',
+			body: data,
+		});
+
+		const responseData = await response.json();
+
+		console.log(responseData);
+
+		if (!response.ok || !Object.keys(responseData).length) throw Error('Unable to upload data');
+
+		// NOTE Redirecting to Stripe
+		const redirectUrl =
+			$('#select-plan').val() === 'standard'
+				? `https://buy.stripe.com/test_6oEg1Wd3q8v1eJy8wF?client_reference_id=${responseData.user_id}`
+				: `https://buy.stripe.com/test_00g8zu3sQfXtbxm008?client_reference_id=${responseData.user_id}`;
+
+		$('#form-block').submit();
+
+		window.location.replace(redirectUrl);
+	} catch (error) {
+		alert('Ocorreu um erro ao preencher o formulário. Por favor, preencha todos os campos!');
+	}
+});
