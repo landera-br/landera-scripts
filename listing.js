cd9dbd06a4be1ba92198446b49343e39322cf2c2
Paulo Coronado <plcpinho@gmail.com>
2022-07-07 21:48:07 -0300

    (fix): fade

--- listing.js
+++ listing.js
@@ -1,11 +1,11 @@
 // NOTE When form is submitted
 $('#btn-interest').on('click', async (e) => {
-	$('#form-modal').css('display', 'flex').hide().fadeIn();
+	$('#form-modal').css('display', 'flex').show();
 
 	$('#btn-interest-close').on('click', async (e) => $('#form-modal').hide());
 
 	$('#btn-interest-submit').on('click', async (e) => {
 		$('#form-modal').hide();
-		$('#form-modal').fadeOut();
+		$('#form-modal').hide();
 	});
 });
