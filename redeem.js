1aac39395388c8bfb52263127e0cd3c3ef3713b3
Paulo Coronado <plcpinho@gmail.com>
2022-07-29 08:46:40 -0300

    (fix): removed landera from redeem options

--- redeem.js
+++ redeem.js
@@ -14,6 +14,8 @@ $(document).ready(async function () {
 		alert('Não foi possível recuperar os dados do cliente.');
 	}
 
+	partners = partners.shift();
+
 	$.each(partners, function (i, partner) {
 		options = options + `<option value="${partner._id}">${partner.name}</option>`;
 
