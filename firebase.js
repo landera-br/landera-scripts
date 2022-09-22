1ba111b503968a0b7636313ec6c836a2bf5f4e4b
Paulo Coronado <plcpinho@gmail.com>
2022-09-22 15:09:28 -0300

    (fix): sdk standard

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-firestore.js';
 
 // import {
