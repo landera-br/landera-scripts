52cc882b090caff481f8a5833e556a5de377e661
Paulo Coronado <plcpinho@gmail.com>
2022-09-22 15:58:42 -0300

    (test): log

--- chat.js
+++ chat.js
@@ -90,6 +90,7 @@ $('.chat-form').submit(async (e) => {
 			text: $('#input-message').val(),
 		});
 	} catch (error) {
+		console.log(error);
 		alert('Não foi possível enviar a mensagem. Por favor, tente novamente mais tarde.');
 	}
 
