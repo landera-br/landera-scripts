497950c9b4e5a4a2a4ca41062ac52d2f6b9f4577
Paulo Coronado <plcpinho@gmail.com>
2022-09-26 12:38:23 -0300

    (fix): main.js with cookies

--- main.js
+++ main.js
@@ -95,6 +95,12 @@ if (btnGoogleSignIn !== null) btnGoogleSignIn.addEventListener('click', googleSi
 if (btnFbSignIn !== null) btnFbSignIn.addEventListener('click', fbSignInHandler, true);
 if (btnPassReset !== null) btnPassReset.addEventListener('click', passwordResetHandler, true);
 
+window.addEventListener('load', function () {
+	setCookie();
+});
+
+if ($('#w3a-container')[0]) $('#w3a-container').css({ position: 'relative', 'z-index': 1001 });
+
 // NOTE Login button handler
 function loginHandler(e) {
 	e.preventDefault();
@@ -321,4 +327,92 @@ async function setUser(uid, email, name) {
 	} catch (error) {
 		alert('Não foi possível recuperar os dados do cliente.');
 	}
+}
+
+// NOTE Stripe Handler
+$('.btn-stripe-session').on('click', async function () {
+	// NOTE Get stripe_session_url
+	try {
+		const response = await fetch(
+			'https://landera-network-7ikj4ovbfa-uc.a.run.app/api/v1/users/sessions/portal',
+			{
+				method: 'POST',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ stripe_customer_id: localStorage.getItem('stripe_customer_id') }),
+			}
+		);
+
+		const responseData = await response.json();
+
+		if (!response.ok || !responseData.stripe_session_url)
+			if (!alert('Não foi possível recuperar os dados do cliente.')) return;
+
+		window.location = responseData.stripe_session_url;
+	} catch (error) {
+		alert('Não foi possível recuperar os dados do cliente.');
+	}
+});
+
+// NOTE Chat Redirect
+$('.btn-chat').on('click', () => {
+	if (localStorage.getItem('wf_inbox_id')) {
+		window.location = `/inbox/${localStorage.getItem('wf_inbox_id')}`;
+	} else {
+		alert(
+			'Não foi possível encontrar as suas conversas. Por favor, entre em contato com o suporte!'
+		);
+	}
+});
+
+function setCookie() {
+	// NOTE Handlers
+	$('.btn-cookie-accept').click(() => {
+		$('#cookie-popup').hide();
+		$('#cookie-preference').hide();
+		document.cookie = 'consent=all';
+		$('#cookie-tab').show();
+	});
+
+	$('.btn-interest-submit').click(() => {
+		$('#cookie-popup').hide();
+		$('#cookie-preference').hide();
+		document.cookie = 'consent=all';
+		$('#cookie-tab').show();
+	});
+
+	$('#btn-cookie-save').click(() => {
+		$('#cookie-preference').hide();
+		document.cookie = 'consent=all';
+		$('#cookie-tab').show();
+	});
+
+	$('.btn-cookie-close').click(() => {
+		$('#cookie-popup').hide();
+		$('#cookie-preference').hide();
+	});
+
+	$('.btn-cookie-settings').click(() => {
+		$('#cookie-popup').hide();
+		$('#cookie-preference').css('display', 'flex').show();
+	});
+
+	// NOTE Cookies
+	if (getCookie('consent')) {
+		$('#cookie-tab').show();
+	} else {
+		$('#cookie-tab').hide();
+		$('#cookie-popup').show();
+	}
+}
+
+function getCookie(cookie) {
+	var cookieArr = document.cookie.split(';');
+	for (var i = 0; i < cookieArr.length; i++) {
+		var cookiePair = cookieArr[i].split('=');
+		if (cookie == cookiePair[0].trim()) return decodeURIComponent(cookiePair[1]);
+	}
+	return null;
 }
