8f4ddbd63d373220041a5d49b84028cfe582ad89
Paulo Coronado <plcpinho@gmail.com>
2023-04-28 15:52:34 -0300

    (test): canvas

--- vision.js
+++ vision.js
@@ -68,6 +68,8 @@ function updateSlides(index = null) {
 			}
 		});
 	}
+
+	activateCanvas();
 	tippy('.tippy');
 }
 
@@ -273,6 +275,120 @@ function addOutputMenu(index) {
 		} else {
 			displayOutput();
 		}
+	});
+}
+
+function activateCanvas() {
+	// get references to the canvas and context
+	var canvas = document.getElementById('canvas');
+	var overlay = document.getElementById('overlay');
+	var ctx = canvas.getContext('2d');
+	var ctxo = overlay.getContext('2d');
+
+	// style the context
+	ctx.strokeStyle = '#3782ff';
+	ctx.lineWidth = 2;
+	ctxo.strokeStyle = '#3782ff';
+	ctxo.lineWidth = 2;
+
+	// calculate where the canvas is on the window
+	// (used to help calculate mouseX/mouseY)
+	var $canvas = $('#canvas');
+	var canvasOffset = $canvas.offset();
+	var offsetX = canvasOffset.left;
+	var offsetY = canvasOffset.top;
+	var scrollX = $canvas.scrollLeft();
+	var scrollY = $canvas.scrollTop();
+
+	// this flage is true when the user is dragging the mouse
+	var isDown = false;
+
+	// these vars will hold the starting mouse position
+	var startX;
+	var startY;
+
+	var prevStartX = 0;
+	var prevStartY = 0;
+
+	var prevWidth = 0;
+	var prevHeight = 0;
+
+	function handleMouseDown(e) {
+		e.preventDefault();
+		e.stopPropagation();
+
+		// save the starting x/y of the rectangle
+		startX = parseInt(e.clientX - offsetX);
+		startY = parseInt(e.clientY - offsetY);
+
+		// set a flag indicating the drag has begun
+		isDown = true;
+	}
+
+	function handleMouseUp(e) {
+		e.preventDefault();
+		e.stopPropagation();
+
+		// the drag is over, clear the dragging flag
+		isDown = false;
+		ctxo.strokeRect(prevStartX, prevStartY, prevWidth, prevHeight);
+	}
+
+	function handleMouseOut(e) {
+		e.preventDefault();
+		e.stopPropagation();
+
+		// the drag is over, clear the dragging flag
+		isDown = false;
+	}
+
+	function handleMouseMove(e) {
+		e.preventDefault();
+		e.stopPropagation();
+
+		// if we're not dragging, just return
+		if (!isDown) {
+			return;
+		}
+
+		// get the current mouse position
+		mouseX = parseInt(e.clientX - offsetX);
+		mouseY = parseInt(e.clientY - offsetY);
+
+		// put your mousemove stuff here
+
+		// calculate the rectangle width/height based
+		// on starting vs current mouse position
+		var width = mouseX - startX;
+		var height = mouseY - startY;
+
+		// clear the canvas
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+		// draw a new rect from the start position
+		// to the current mouse position
+		ctx.strokeRect(startX, startY, width, height);
+
+		prevStartX = startX;
+		prevStartY = startY;
+
+		prevWidth = width;
+		prevHeight = height;
+	}
+
+	// listen for mouse events
+	$('#canvas').mousedown(function (e) {
+		handleMouseDown(e);
+	});
+	$('#canvas').mousemove(function (e) {
+		handleMouseMove(e);
+	});
+	$('#canvas').mouseup(function (e) {
+		handleMouseUp(e);
+	});
+
+	$('#canvas').mouseout(function (e) {
+		handleMouseOut(e);
 	});
 }
 
