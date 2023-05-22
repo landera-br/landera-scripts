0fc4f0b169e12c771aeebf657cb24e3dff6b728e
Paulo Coronado <plcpinho@gmail.com>
2023-05-22 11:24:09 -0300

    (test)

--- vision.js
+++ vision.js
@@ -407,6 +407,7 @@ function updateCanvasListeners() {
 	// Add event listeners to the current canvas
 	if (elements[current_slide].length < MAX_BOXES) {
 		const currentCanvas = canvas[current_slide];
+		console.log(currentCanvas);
 		currentCanvas.addEventListener('mousedown', handleMouseDown.bind(null, currentCanvas));
 		currentCanvas.addEventListener('mousemove', handleMouseMove.bind(null, currentCanvas));
 		currentCanvas.addEventListener('mouseup', handleMouseUp.bind(null, currentCanvas));
